763e97a01ecbd464a7c596971c5075a7c85c6451
Tim Goalen <timgoalen@hotmail.com>
2023-03-23 14:15:47 +0000

    Add navbar javascript functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Hamburger menu toggle, from https://www.youtube.com/watch?v=c9jNIYQ1IuI
+
 const responsiveMenuButton = document.getElementById('responsiveMenuToggleButton');
 
 const navBarLinks = document.querySelector('.navbar-links');
@@ -7,12 +9,23 @@ responsiveMenuButton.addEventListener('click', () => {
     responsiveMenuButton.classList.toggle('open');
 });
 
-// Pause all audio script, from
+// Close menu when an item is clicked
 
-document.addEventListener('play', function(e){
+const allNavLinks = document.querySelectorAll('.navbar-links li');
+
+allNavLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navBarLinks.classList.remove('open');
+        responsiveMenuButton.classList.remove('open');
+    })
+})
+
+// Pause all audio script, from https://stackoverflow.com/questions/19790506/multiple-audio-html-auto-stop-other-when-current-is-playing-with-javascript
+
+document.addEventListener('play', function (e) {
     var audios = document.getElementsByTagName('audio');
-    for(var i = 0, len = audios.length; i < len;i++){
-        if(audios[i] != e.target){
+    for (var i = 0, len = audios.length; i < len; i++) {
+        if (audios[i] != e.target) {
             audios[i].pause();
         }
     }
