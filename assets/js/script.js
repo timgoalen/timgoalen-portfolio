979bb848011008c0989d031857d042d923cc5cb6
Tim Goalen <timgoalen@hotmail.com>
2023-03-28 11:13:29 +0000

    Add active class style and javascript to nav items

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,26 @@ document.addEventListener('play', function (e) {
         }
     }
 }, true);
+
+/* Underline active section solution from https://www.w3schools.com/howto/howto_js_active_element.asp */
+
+// Get the container element
+var btnContainer = document.getElementById("nav-links");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
