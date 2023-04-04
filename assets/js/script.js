c3df3d2093f52fb42e259c527da0b1272d25dda6
Tim Goalen <timgoalen@hotmail.com>
2023-04-04 12:56:43 +0000

    Reorder css and js for clarity

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ const responsiveMenuButton = document.getElementById('responsiveMenuToggleButton
 const navBarLinks = document.querySelector('.navbar-links');
 
 responsiveMenuButton.addEventListener('click', () => {
-    navBarLinks.classList.toggle('open');
-    responsiveMenuButton.classList.toggle('open');
+  navBarLinks.classList.toggle('open');
+  responsiveMenuButton.classList.toggle('open');
 });
 
 // Close menu when an item is clicked
@@ -14,24 +14,24 @@ responsiveMenuButton.addEventListener('click', () => {
 const allNavLinks = document.querySelectorAll('.navbar-links li');
 
 allNavLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navBarLinks.classList.remove('open');
-        responsiveMenuButton.classList.remove('open');
-    })
+  link.addEventListener('click', () => {
+    navBarLinks.classList.remove('open');
+    responsiveMenuButton.classList.remove('open');
+  })
 })
 
 // Pause all audio script, from https://stackoverflow.com/questions/19790506/multiple-audio-html-auto-stop-other-when-current-is-playing-with-javascript
 
 document.addEventListener('play', function (e) {
-    var audios = document.getElementsByTagName('audio');
-    for (var i = 0, len = audios.length; i < len; i++) {
-        if (audios[i] != e.target) {
-            audios[i].pause();
-        }
+  var audios = document.getElementsByTagName('audio');
+  for (var i = 0, len = audios.length; i < len; i++) {
+    if (audios[i] != e.target) {
+      audios[i].pause();
     }
+  }
 }, true);
 
-/* Underline active section solution from https://www.w3schools.com/howto/howto_js_active_element.asp */
+// Underline active section solution from https://www.w3schools.com/howto/howto_js_active_element.asp
 
 // Get the container element
 var btnContainer = document.getElementById("nav-links");
@@ -41,7 +41,7 @@ var btns = btnContainer.getElementsByClassName("btn");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
 
     // If there's no active class
